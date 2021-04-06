ba0eb75b7c696653f27617dbc524663042a02555
SOMANI <OJ96DAd@ad.ing.net>
2021-04-06 19:58:50 +0200

    feat(app-frame): Added app frame component

--- src/components/app-frame/AppFrame.test.tsx
+++ src/components/app-frame/AppFrame.test.tsx
@@ -3,19 +3,19 @@ import { render, screen } from '@testing-library/react';
 import AppFrame from './AppFrame';
 
 test('should have a header section', () => {
-  render(<AppFrame />);
-  const headerElement = screen.getByRole('header');
+  render(<AppFrame headerLeft={<div>Header</div>} headerRight={<div></div>} footer={<div></div>}/>);
+  const headerElement = screen.getByText('Header');
   expect(headerElement).toBeInTheDocument();
 });
 
 test('should have a container section', () => {
-    render(<AppFrame />);
+    render(<AppFrame headerLeft={<div></div>} headerRight={<div></div>} footer={<div></div>}/>);
     const mainElement = screen.getByRole('main');
     expect(mainElement).toBeInTheDocument();
 });
 
 test('should have a footer section', () => {
-    render(<AppFrame />);
-    const footerElement = screen.getByRole('footer');
+    render(<AppFrame headerLeft={<div></div>} headerRight={<div></div>} footer={<div>Footer</div>}/>);
+    const footerElement = screen.getByText('Footer');
     expect(footerElement).toBeInTheDocument();
 });
--- src/components/app-frame/AppFrame.tsx
+++ src/components/app-frame/AppFrame.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
-const AppFrame = () => <div></div>
+type AppFrameProps = {
+    headerLeft: JSX.Element,
+    headerRight: JSX.Element,
+    footer: JSX.Element
+}
+
+const AppFrame: FunctionComponent<AppFrameProps> = (props) => {
+    return (
+        <div className="page-container">
+            <header className="page__header">
+                <section className="header__left">
+                    {props.headerLeft}
+                </section>
+                <section className="header__right">
+                    {props.headerRight}
+                </section>
+            </header>
+            <main className="content">
+                {props.children}
+            </main>
+            <footer className="footer">
+                {props.footer}
+            </footer>
+        </div>
+    )
+}
 
 export default AppFrame;
